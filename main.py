a1ffbe8c34dc4828c1dea9e6ba465b3c2dbdbea3
YongWook <ywha12@gmail.com>
2018-06-26 13:31:01 +0900

    add discriminator

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from utils import pp, visualize, to_json
 import tensorflow as tf
 
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 70, "Epoch to train [25]")
+flags.DEFINE_integer("epoch", 210, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("train_size", 9999999, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
 flags.DEFINE_integer("image_size", 128, "The size of image to use [128]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("checkpoint_load_dir", "None", "Directory name to save the checkpoints [checkpoint]")
 flags.DEFINE_string("dataset", "128by128", "name of data set to use")
 flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
 flags.DEFINE_boolean("is_train", True, "True for training, False for testing [False]")
@@ -34,7 +35,8 @@ def main(_):
                       image_size=FLAGS.image_size,
                       batch_size=FLAGS.batch_size,
                       dataset=FLAGS.dataset,
-                      checkpoint_dir=FLAGS.checkpoint_dir)
+                      checkpoint_dir=FLAGS.checkpoint_dir,
+                      checkpoint_load_dir=FLAGS.checkpoint_load_dir)
 
         if FLAGS.is_train:
             dcgan.train(FLAGS)
